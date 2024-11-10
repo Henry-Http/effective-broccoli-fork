eae816d8f0ebc7094bc174740eb7a09b2b3a562e
Adeola Adekunle <adeola@emtech.com>
2024-11-10 08:08:55 +0100

    added find nft to free urls

--- src/main/java/com/nft/nftsite/security/AllowedURIs.java
+++ src/main/java/com/nft/nftsite/security/AllowedURIs.java
@@ -14,20 +14,10 @@ public class AllowedURIs {
             "/api/v1/user/reset-password/complete", "/api/v1/user/third-party-signin", // all users
             "/api/v1/admin/setup", // admin
             "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", // swagger
-            "/api/v1/movie/search", "/api/v1/movie/sitemap", // movie
-            "/api/v1/film/trending", "/api/v1/film/slug/**", "/api/v1/film/latest-release", "/api/v1/film/coming-soon", "/api/v1/film/filter", // films
-            "/api/v1/series/trending", "/api/v1/series/slug/**", "/api/v1/series/latest-release", // series
-            "/api/v1/trailer/slug/**", "/api/v1/trailer/movie/**", "/api/v1/trailer/coming-soon", "/api/v1/trailer/now-showing", // trailers
-            "/api/v1/movie-role/movie/**", "/api/v1/movie-role/person/**", // movie roles
-            "/api/v1/person/slug/**", "/api/v1/person/sitemap", // persons (actors and filmmakers)
-            "/api/v1/review/movie/**",
-            "/api/v1/webhook/squad", "/api/v1/recommendation/hero-movie/all",
-            "/api/v1/corner/search", "/api/v1/corner/recommend",
-            "/api/v1/post/spotlight", "/api/v1/post/slug/**", "/api/v1/post/sitemap", "/api/v1/post/public",
             "/api/v1/poll/**", // polls
             "/socket.io", "/socket.io/**",
             "/api/v1/corner/sitemap", "/api/v1/corner/post/sitemap",
-            "/api/v1/comment/**" // post comments
+            "/api/v1/nft/find", "/api/v1/nft/find/**"
     };
 
     public static boolean isAllowed(String uri) {
