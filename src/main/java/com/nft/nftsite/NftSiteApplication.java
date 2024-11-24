1d76bda70bd24df8025974b2deb367ade2843583
Adeola Adekunle <adeola@emtech.com>
2024-11-24 19:51:58 +0100

    enable async

--- src/main/java/com/nft/nftsite/NftSiteApplication.java
+++ src/main/java/com/nft/nftsite/NftSiteApplication.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @OpenAPIDefinition(
@@ -47,6 +48,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 )
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 public class NftSiteApplication {
 
     public static void main(String[] args) {
